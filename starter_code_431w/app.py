9a2ecc4928ffe6b78dd6f186dfe1c486cd237ae9
Matthew Gurer <mpg6020@psu.edu>
2026-02-23 17:07:22 -0500

    Fully working app without bonus features

--- starter_code_431w/app.py
+++ starter_code_431w/app.py
@@ -14,23 +14,48 @@ def index():
 @app.route('/name', methods=['POST', 'GET'])
 def name():
     error = None
+    result = get_database()
     if request.method == 'POST':
         result = valid_name(request.form['FirstName'], request.form['LastName'])
         if result:
             return render_template('input.html', error=error, result=result)
         else:
             error = 'invalid input name'
-    return render_template('input.html', error=error)
+    return render_template('input.html', error=error, result=result)
+
+@app.route('/delete', methods=['POST', 'GET'])
+def delete():
+    error = None
+    result = get_database()
+    if request.method == 'POST':
+        result = valid_delete(request.form['FirstName'], request.form['LastName'])
+        if result:
+            return render_template('delete.html', error=error, result=result)
+        else:
+            error = 'invalid input name'
+    return render_template('delete.html', error=error, result=result)
 
 
 def valid_name(first_name, last_name):
     connection = sql.connect('database.db')
-    connection.execute('CREATE TABLE IF NOT EXISTS users(firstname TEXT, lastname TEXT);')
+    connection.execute('CREATE TABLE IF NOT EXISTS users(pid INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT);')
     connection.execute('INSERT INTO users (firstname, lastname) VALUES (?,?);', (first_name, last_name))
     connection.commit()
     cursor = connection.execute('SELECT * FROM users;')
     return cursor.fetchall()
 
+def valid_delete(first_name, last_name):
+    connection = sql.connect('database.db')
+    connection.execute('CREATE TABLE IF NOT EXISTS users(pid INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, lastname TEXT);')
+    connection.execute('DELETE FROM users WHERE firstname=? AND lastname=?;', (first_name, last_name))
+    connection.commit()
+    cursor = connection.execute('SELECT * FROM users;')
+    return cursor.fetchall()
+
+def get_database():
+    connection = sql.connect('database.db')
+    cursor = connection.execute('SELECT * FROM users;')
+    return cursor.fetchall()
 
 if __name__ == "__main__":
     app.run()
